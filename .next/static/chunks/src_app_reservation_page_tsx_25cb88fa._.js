a8f1cca72dae6c2998443b10a46d35e97d749390
michaelAk-hub <mkoutoumbas@gmail.com>
2025-08-18 00:06:32 +0300

    Add .next/static/chunks/src_app_reservation_page_tsx_25cb88fa._.js 1755464791828

---
+++ .next/static/chunks/src_app_reservation_page_tsx_25cb88fa._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_6c5f5fb8._.js",
+  "static/chunks/src_64c5a024._.js"
+],
+    source: "dynamic"
+});
